51550edc661737419624bdad6eeda386a41cee3d
Daniel Lublin <daniel@lublin.se>
2017-11-18 17:54:32 +0100

    descs: shoving it in there
    Thx to martin

--- lib-descs.js
+++ lib-descs.js
@@ -7,8 +7,19 @@
 
 var descs = {
   "18d6c72a0cc4": "quite",
-  "18d6c7453ec4": "möllan.lulle",
+
   "18a6f7f04a06": "möllan.moa",
+  "18d6c7453ec4": "möllan.lulle",
+
+  "18a6f7f04eae": "kalle",
+  "18d6c72a100a": "claesg-sofiel", // mot fiskart
+  "8416f92db85a": "canyon",
+  "c025e9a84c8a": "fisk.cykelson",
+  "c025e999e6e4": "fisk.istanb?",
+  "8416f92daa8e": "fisk.salongfisk",
+  "c025e999ea9a": "fisk.thaithai1",
+  "c025e999e6ea": "fisk.thaithai2",
+
   "8416f92daa22": "erik1",
   "18d6c72a0efe": "erik2",
   "8416f9e45668": "erik3",
@@ -17,26 +28,32 @@ var descs = {
   "18d6c72a0bc0": "fredr.fisk",
   "18d6c72a0eb6": "fredr.stat",
   "8416f92db8da": "haweli?",
-  "18a6f7f04eae": "kalle",
-  "18d6c72a100a": "claesg-sofiel", // mot fiskart
-  "c025e9a84c8a": "fisk.cykel",
-  "c025e999e6e4": "fisk.istanb?",
-  "18d6c72a0ecc": "martin.nets",
-  "18d6c72a0a04": "martin.akut",
-  "18d6c72a0196": "martin.choklad",
-  "18d6c72a1008": "kami",
+
+  "8416f92a6f18": "söfö.daniel",
   "18d6c74551f8": "söfö.lgh1201",
   "18d6c72a08e0": "justnu",
   "8416f92db8a0": "ink-on-skin", //2e6f312f66f0
-  "8416f92a6f18": "söfö.danne",
+  "8416f92db832": "gorgeous",
   "c025e99a14be": "mio", //66126cc0acf8
-  "18d6c72a0db0": "silwer", //8a750390e2e8
+  "8416f9e39ae6": "triang.ammo",
+  "18d6c72a0db0": "silwertobak", //8a750390e2e8
   "c025e9a84baa": "kökskomp", //b26d326008b0
-  "18d6c72a0b30": "triangelnstobak", //2262d1f72478
+  "18d6c72a0b30": "triang.tobak", //2262d1f72478
   "18d6c729ff48": "triang.gabi1",
   "18d6c7453e5c": "triang.gabi2",
+  "18a6f7f046c2": "gåg.pontus",
+  "8416f92da9e6": "carlings",
   "8416f9ae6650": "triang.håkan",
-  "18a6f7f046c2": "triang.pontus",
+
+  "18d6c72a1008": "kami",
+  "18d6c72a0ecc": "zeo",
+  "18d6c72a0a04": "zeo.nikolaig",
+  "18d6c72a0196": "zeo.mölleg",
+  "18d6c72a0bb0": "mölleg.chokladf",
+  "18d6c729ff5a": "bergsg.chokladf",
+  "c025e9a84b32": "bergsg.sara",
+  "c025e999e70e": "mölleg.hojen",
+  "c025e99a0e3a": "friisg.hojen",
 };
 
 function getnodedesc(nodeid) {
