7fce17d7de04818efa3019f26d094cbc1e3fe4d9
Daniel Lublin <daniel@lublin.se>
2017-11-18 12:15:46 +0100

    descs: reorder, remove dupes, adjust slightly

--- lib-descs.js
+++ lib-descs.js
@@ -6,7 +6,7 @@
 // `tools/bssid-to-nodeid` to get hold of the node_id to be used here.
 
 var descs = {
-  "18d6c72a0cc4": "daniel",
+  "18d6c72a0cc4": "quite",
   "18d6c7453ec4": "möllan.lulle",
   "18a6f7f04a06": "möllan.moa",
   "8416f92daa22": "erik1",
@@ -18,27 +18,25 @@ var descs = {
   "18d6c72a0eb6": "fredr.stat",
   "8416f92db8da": "haweli?",
   "18a6f7f04eae": "kalle",
-  "18d6c72a100a": "claesg.sofiel", // mot fiskart
+  "18d6c72a100a": "claesg-sofiel", // mot fiskart
   "c025e9a84c8a": "fisk.cykel",
   "c025e999e6e4": "fisk.istanb?",
   "18d6c72a0ecc": "martin.nets",
   "18d6c72a0a04": "martin.akut",
   "18d6c72a0196": "martin.choklad",
   "18d6c72a1008": "kami",
-  "8416f92a6f18": "söfö.danne",
-  "18d6c72a08e0": "söfö.justnu", // ? black ink tattoo?
   "18d6c74551f8": "söfö.lgh1201",
+  "18d6c72a08e0": "justnu",
+  "8416f92db8a0": "ink-on-skin", //2e6f312f66f0
+  "8416f92a6f18": "söfö.danne",
+  "c025e99a14be": "mio", //66126cc0acf8
+  "18d6c72a0db0": "silwer", //8a750390e2e8
+  "c025e9a84baa": "kökskomp", //b26d326008b0
+  "18d6c72a0b30": "triangelnstobak", //2262d1f72478
   "18d6c729ff48": "triang.gabi1",
   "18d6c7453e5c": "triang.gabi2",
   "8416f9ae6650": "triang.håkan",
-  "18a6f7f046c2": "pontus",
-  "8416f92db8a0": "inkonskintattoo", //2e6f312f66f0
-  "18d6c72a08e0": "justnu", //1627ed37c610
-  "8416f92a6f18": "daniel?", //8416f92a6f18
-  "18d6c72a0b30": "triangelnstobak", //2262d1f72478
-  "c025e9a84baa": "kökskompaniet", //b26d326008b0
-  "c025e99a14be": "mio", //66126cc0acf8
-  "18d6c72a0db0": "silwertobak", //8a750390e2e8
+  "18a6f7f046c2": "triang.pontus",
 };
 
 function getnodedesc(nodeid) {
