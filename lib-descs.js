38f374105a78b9c5dcdb1620b037b168d2709492
martinsjoborg <martin@zeocore.com>
2018-01-18 14:51:00 +0100

    Update lib-descs.js

--- lib-descs.js
+++ lib-descs.js
@@ -59,7 +59,7 @@ var descs = {
   "18d6c72a1008": "kami",
   "18d6c72a0ecc": "zeo",
   "18d6c72a0a04": "zeo.nikolaig",
-  "18d6c72a0196": "",
+  "8416f92db6d8": "zeo.mölleg",
   "18d6c72a0bb0": "mölleg.chokladf",
   "18d6c729ff5a": "bergsg.chokladf",
   "c025e9a84b32": "bergsg.sara",
