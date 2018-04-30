25da4a5ed146c3aaa5e5395bd3f851d0a97cbf12
martinsjoborg <martin@zeocore.com>
2018-04-30 21:22:45 +0200

    replaced blanda with m2 on nikolaig

--- lib-descs.js
+++ lib-descs.js
@@ -62,7 +62,7 @@ var descs = {
 
   "18d6c72a1008": "kami",
   "18d6c72a0c28": "zeo",
-  "18d6c72a0a04": "zeo.nikolaig",
+  "00156deeeb3c": "zeo.nikolaig",
   "8416f92db6d8": "zeo.mölleg",
   "18d6c72a0bb0": "mölleg.chokladf",
   "18d6c729ff5a": "bergsg.chokladf",
