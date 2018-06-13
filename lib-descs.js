6c47557898b42384b325a3e8df9c195247c67232
omni <omni@pjodd.se>
2018-06-13 19:37:42 +0000

    Add nodes at Amalthea Bokcafé

--- lib-descs.js
+++ lib-descs.js
@@ -101,6 +101,8 @@ var descs = {
 
   "18a6f790f936": "kristianstadsg.ost",
   "8416f9ae9df8": "kristianstadsg.c", //martins polares granne
+  "a0f3c1969988": "amalthea.kg",
+  "a0f3c1823ec2": "amalthea.nv",
 
   "18d6c72a125e": "5hUr1k3n",
 
