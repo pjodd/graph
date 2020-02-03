dc6cc59044c08d23329c7ad415dec55ae94d6842
Daniel Lublin <daniel@lublin.se>
2020-02-03 17:13:38 +0100

    Hash also 3rd party js

--- build.sh
+++ build.sh
@@ -4,21 +4,22 @@ set -euo pipefail
 BUILDDIR=${1}
 
 HTML=(graph.html nodes.html)
-ASSET=(index.html d3.min.js d3-queue.min.js p.png)
+ASSET=(index.html p.png)
 CSS=(graph.css)
 JS=(graph.js nodes.js)
 JSLIB=(lib-descs.js lib-funcs.js)
+JS3RD=(d3.min.js d3-queue.min.js)
 
 mkdir -p "$BUILDDIR"
 
-cp -af "${HTML[@]}" "${CSS[@]}" "${ASSET[@]}" "$BUILDDIR"
+cp -af "${HTML[@]}" "${ASSET[@]}" "${CSS[@]}" "${JS3RD[@]}" "$BUILDDIR"
 
 for f in "${JS[@]}"; do
   cat "${JSLIB[@]}" "$f" | grep -v "^[[:space:]]*//" >"$BUILDDIR/$f"
 done
 
 cd "$BUILDDIR"
-for f in "${CSS[@]}" "${JS[@]}"; do
+for f in "${CSS[@]}" "${JS[@]}" "${JS3RD[@]}"; do
   hash=$(md5sum <"$f" | cut -b 1-8)
   [[ -z "$hash" ]] && { echo "$f: hash: $hash"; exit 1; }
   hashedf="${f%.*},$hash.${f##*.}"
